37f87e5ec09c9a47b938ed9459fec6729e0402cc
consing <consing3@naver.com>
2017-04-06 16:03:31 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,329 @@
+<html>
+	<head>
+		<title>Music</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/boostrap.re-adjust.css" />
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+<style>
+audio{
+    background: repeat-x 0 -80px;
+    width: 100%;
+    height: 40px;
+    position: relative;
+    font: normal 13px Arial, sans-serif;
+    margin-bottom: 24px;
+}
+</style>
+	</head>
+	<body>
+			<div id="content">
+		<?php
+ 		require("/menu.html");
+ 		?>
+		</div>
+		<div class="main" style="width:960px;  margin: 0 auto;">
+
+		<div class="panel panel-default bgmPlayWrap">
+    <div class="panel-heading clearfix">Miku Song<a href="?sort_by=date&amp;sort_type=desc" class="pull-right">더 보기»</a>
+    </div>
+    <table class="table table-hover table-condensed" style="padding: 0px; margin:0px">
+    <thead>
+    <tr>
+        <td class="Recommendation">
+            추천
+        </td>
+        <td class="Title">
+            제목 & 업로자
+        </td>
+        <!--<td class="Popular">
+            인기도
+        </td>-->
+    </tr>
+    </thead>
+    <tbody>
+			<tr class="Miku" data-order="0" data-num="256633" data-key="v0yNj" data-is-playing="false">
+        <td class="tblBodyVote">
+            <button class="btn btn-success btn-sm btnVote ladda-button" data-style="zoom-out" title="추천"><span class="ladda-label"><span class="glyphicon glyphicon-thumbs-up"></span></span></button>
+        </td>
+        <td class="tblBodyTitle">
+            <div class="clearfix" style="word-break:break-all;">
+                <a href="#" class="title">HatsuneMiku - 안녕이란 말 대신, 꽃다발을</a>
+                    <span class="nickname" style="font-size:12px;">& 닉네임 : <a href="#" target="_blank" title="누구냐">누구냐</a></span>
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-default btn-xs dropdown-toggle btnDownload" data-toggle="dropdown">
+                        다운로드 <span class="glyphicon glyphicon-cloud-download"></span>
+                        </button>
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                            <a class="btnDownloadInner" href="#">mp3 파일로 다운로드</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <span class="label label-warning">NEW</span>
+                <audio id="audio" preload="none" tabindex="0" controls="" src="music/Vocaloid/Miku/HatsuneMiku - 안녕이란 말 대신, 꽃다발을.mp3">
+                </audio>
+            <div class="addit clearfix">
+                </div>
+            </div>
+
+            <tr class="Miku" data-order="0" data-num="256633" data-key="v0yNj" data-is-playing="false">
+        <td class="tblBodyVote">
+            <button class="btn btn-success btn-sm btnVote ladda-button" data-style="zoom-out" title="추천"><span class="ladda-label"><span class="glyphicon glyphicon-thumbs-up"></span></span></button>
+        </td>
+        <td class="tblBodyTitle">
+            <div class="clearfix" style="word-break:break-all;">
+                <a href="#" class="title">HatsuneMiku - TABOO</a>
+                    <span class="nickname" style="font-size:12px;">& 닉네임 : <a href="#" target="_blank" title="누구냐">누구냐</a></span>
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-default btn-xs dropdown-toggle btnDownload" data-toggle="dropdown">
+                        다운로드 <span class="glyphicon glyphicon-cloud-download"></span>
+                        </button>
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                            <a class="btnDownloadInner" href="#">mp3 파일로 다운로드</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <span class="label label-warning">NEW</span>
+                <audio id="audio" preload="none" tabindex="0" controls="" src="music/Vocaloid/Miku/HatsuneMiku - TABOO.mp3">
+                </audio>
+            </div>
+
+            <tr class="Miku" data-order="0" data-num="256633" data-key="v0yNj" data-is-playing="false">
+        <td class="tblBodyVote">
+            <button class="btn btn-success btn-sm btnVote ladda-button" data-style="zoom-out" title="추천"><span class="ladda-label"><span class="glyphicon glyphicon-thumbs-up"></span></span></button>
+        </td>
+        <td class="tblBodyTitle">
+            <div class="clearfix" style="word-break:break-all;">
+                <a href="#" class="title">Blessing feat. VOCALOIDS.mp3</a>
+                    <span class="nickname" style="font-size:12px;">& 닉네임 : <a href="#" target="_blank" title="누구냐">누구냐</a></span>
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-default btn-xs dropdown-toggle btnDownload" data-toggle="dropdown">
+                        다운로드 <span class="glyphicon glyphicon-cloud-download"></span>
+                        </button>
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                            <a class="btnDownloadInner" href="#">mp3 파일로 다운로드</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <span class="label label-warning">NEW</span>
+                <audio id="audio" preload="none" tabindex="0" controls="" src="music/Vocaloid/Miku/Blessing feat. VOCALOIDS.mp3">
+                </audio>
+            </div>
+
+            <tr class="Miku" data-order="0" data-num="256633" data-key="v0yNj" data-is-playing="false">
+        <td class="tblBodyVote">
+            <button class="btn btn-success btn-sm btnVote ladda-button" data-style="zoom-out" title="추천"><span class="ladda-label"><span class="glyphicon glyphicon-thumbs-up"></span></span></button>
+        </td>
+        <td class="tblBodyTitle">
+            <div class="clearfix" style="word-break:break-all;">
+                <a href="#" class="title">2M+3M MIX.mp3</a>
+                    <span class="nickname" style="font-size:12px;">& 닉네임 : <a href="#" target="_blank" title="누구냐">누구냐</a></span>
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-default btn-xs dropdown-toggle btnDownload" data-toggle="dropdown">
+                        다운로드 <span class="glyphicon glyphicon-cloud-download"></span>
+                        </button>
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                            <a class="btnDownloadInner" href="#">mp3 파일로 다운로드</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <span class="label label-warning">NEW</span>
+                <audio id="audio" preload="none" tabindex="0" controls="" src="music/Vocaloid/Miku/2M+3M MIX.mp3">
+                </audio>
+            </div>
+            </tbody>
+            </table>
+        </div>
+
+        <div class="panel panel-default bgmPlayWrap">
+    <div class="panel-heading clearfix">
+         Luka Song <a href="/?sort_by=date&amp;sort_type=desc" class="pull-right">더 보기»</a>
+    </div>
+    <table class="table table-hover table-condensed" style="padding: 0px; margin:0px">
+    <thead>
+    <tr>
+        <td class="tblHeadVote">
+            추천
+        </td>
+        <td class="tblHeadTitle">
+            제목 & 업로자
+        </td>
+        <!--<td class="tblHeadPopular">
+            인기도
+        </td>-->
+    </tr>
+    </thead>
+    <tbody>
+			<tr class="Luka" data-order="0" data-num="256633" data-key="v0yNj" data-is-playing="false">
+        <td class="tblBodyVote">
+            <button class="btn btn-success btn-sm btnVote ladda-button" data-style="zoom-out" title="추천"><span class="ladda-label"><span class="glyphicon glyphicon-thumbs-up"></span></span></button>
+        </td>
+        <td class="tblBodyTitle">
+            <div class="clearfix" style="word-break:break-all;">
+                <a href="#" class="title">메구리네 루카 - Gravity=Reality</a>
+                    <span class="nickname" style="font-size:12px;">& 닉네임 : <a href="#" target="_blank" title="누구냐">누구냐</a></span>
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-default btn-xs dropdown-toggle btnDownload" data-toggle="dropdown">
+                        다운로드 <span class="glyphicon glyphicon-cloud-download"></span>
+                        </button>
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                            <a class="btnDownloadInner" href="#">mp3 파일로 다운로드</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <span class="label label-warning">NEW</span>
+                <audio id="audio" preload="none" tabindex="0" controls="" src="music/Vocaloid/Luka/메구리네 루카 - Gravity=Reality.mp3">
+                </audio>
+            <div class="addit clearfix">
+                </div>
+            </div>
+
+            <tr class="Luka" data-order="0" data-num="256633" data-key="v0yNj" data-is-playing="false">
+        <td class="tblBodyVote">
+            <button class="btn btn-success btn-sm btnVote ladda-button" data-style="zoom-out" title="추천"><span class="ladda-label"><span class="glyphicon glyphicon-thumbs-up"></span></span></button>
+        </td>
+        <td class="tblBodyTitle">
+            <div class="clearfix" style="word-break:break-all;">
+                <a href="#" class="title">메구리네 루카 - 경보!뿌루룽 지령</a>
+                    <span class="nickname" style="font-size:12px;">& 닉네임 : <a href="#" target="_blank" title="누구냐">누구냐</a></span>
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-default btn-xs dropdown-toggle btnDownload" data-toggle="dropdown">
+                        다운로드 <span class="glyphicon glyphicon-cloud-download"></span>
+                        </button>
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                            <a class="btnDownloadInner" href="#">mp3 파일로 다운로드</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <span class="label label-warning">NEW</span>
+                <audio id="audio" preload="none" tabindex="0" controls="" src="music/Vocaloid/Luka/메구리네 루카 - 경보!뿌루룽 지령.mp3">
+                </audio>
+            </div>
+
+            <tr class="Luka" data-order="0" data-num="256633" data-key="v0yNj" data-is-playing="false">
+        <td class="tblBodyVote">
+            <button class="btn btn-success btn-sm btnVote ladda-button" data-style="zoom-out" title="추천"><span class="ladda-label"><span class="glyphicon glyphicon-thumbs-up"></span></span></button>
+        </td>
+        <td class="tblBodyTitle">
+            <div class="clearfix" style="word-break:break-all;">
+                <a href="#" class="title">메구리네 루카 - 루카님은 우울</a>
+                    <span class="nickname" style="font-size:12px;">& 닉네임 : <a href="#" target="_blank" title="누구냐">누구냐</a></span>
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-default btn-xs dropdown-toggle btnDownload" data-toggle="dropdown">
+                        다운로드 <span class="glyphicon glyphicon-cloud-download"></span>
+                        </button>
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                            <a class="btnDownloadInner" href="#">mp3 파일로 다운로드</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <span class="label label-warning">NEW</span>
+                <audio id="audio" preload="none" tabindex="0" controls="" src="music/Vocaloid/Luka/메구리네 루카 - 루카님은 우울.mp3">
+                </audio>
+            </div>
+
+        </tbody>
+    </table>
+</div>
+
+<div class="panel panel-default bgmPlayWrap">
+    <div class="panel-heading clearfix">
+        Ani - ETC <a href="/?sort_by=date&amp;sort_type=desc" class="pull-right">더 보기»</a>
+    </div>
+    <table class="table table-hover table-condensed" style="padding: 0px; margin:0px">
+    <thead>
+    <tr>
+        <td class="tblHeadVote">
+            추천
+        </td>
+        <td class="tblHeadTitle">
+            제목 & 업로자
+        </td>
+        <!--<td class="tblHeadPopular">
+            인기도
+        </td>-->
+    </tr>
+    </thead>
+    <tbody>
+			<tr class="bgmInfo " data-order="0" data-num="256633" data-key="v0yNj" data-is-playing="false">
+        <td class="tblBodyVote">
+            <button class="btn btn-success btn-sm btnVote ladda-button" data-style="zoom-out" title="추천"><span class="ladda-label"><span class="glyphicon glyphicon-thumbs-up"></span></span></button>
+        </td>
+        <td class="tblBodyTitle">
+            <div class="clearfix" style="word-break:break-all;">
+                <a href="#" class="title">Nekopara ネコぱら Vol.1 OP Taiyou Paradise</a>
+                    <span class="nickname" style="font-size:12px;">& 닉네임 : <a href="#" target="_blank" title="누구냐">누구냐</a></span>
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-default btn-xs dropdown-toggle btnDownload" data-toggle="dropdown">
+                        다운로드 <span class="glyphicon glyphicon-cloud-download"></span>
+                        </button>
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                            <a class="btnDownloadInner" href="#">mp3 파일로 다운로드</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <span class="label label-warning">NEW</span>
+                <audio id="audio" preload="none" tabindex="0" controls="" src="music/Ani/Etc/Nekopara ネコぱら Vol.1 OP Taiyou Paradise.mp3">
+                </audio>
+            </div>
+
+            <tr class="bgmInfo " data-order="0" data-num="256633" data-key="v0yNj" data-is-playing="false">
+        <td class="tblBodyVote">
+            <button class="btn btn-success btn-sm btnVote ladda-button" data-style="zoom-out" title="추천"><span class="ladda-label"><span class="glyphicon glyphicon-thumbs-up"></span></span></button>
+        </td>
+        <td class="tblBodyTitle">
+            <div class="clearfix" style="word-break:break-all;">
+                <a href="#" class="title">1 ドキドキ☆ココロ☆フレーバー</a>
+                    <span class="nickname" style="font-size:12px;">& 닉네임 : <a href="#" target="_blank" title="누구냐">누구냐</a></span>
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-default btn-xs dropdown-toggle btnDownload" data-toggle="dropdown">
+                        다운로드 <span class="glyphicon glyphicon-cloud-download"></span>
+                        </button>
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                            <a class="btnDownloadInner" href="#">mp3 파일로 다운로드</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <span class="label label-warning">NEW</span>
+                <audio id="audio" preload="none" tabindex="0" controls="" src="music/Ani/Etc/1 ドキドキ☆ココロ☆フレーバー.mp3">
+                </audio>
+            </div>
+
+              <tr class="bgmInfo " data-order="0" data-num="256633" data-key="v0yNj" data-is-playing="false">
+        <td class="tblBodyVote">
+            <button class="btn btn-success btn-sm btnVote ladda-button" data-style="zoom-out" title="추천"><span class="ladda-label"><span class="glyphicon glyphicon-thumbs-up"></span></span></button>
+        </td>
+        <td class="tblBodyTitle">
+            <div class="clearfix" style="word-break:break-all;">
+                <a href="#" class="title">2 100にゃんパワーで夢心地</a>
+                    <span class="nickname" style="font-size:12px;">& 닉네임 : <a href="#" target="_blank" title="누구냐">누구냐</a></span>
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-default btn-xs dropdown-toggle btnDownload" data-toggle="dropdown">
+                        다운로드 <span class="glyphicon glyphicon-cloud-download"></span>
+                        </button>
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                            <a class="btnDownloadInner" href="#">mp3 파일로 다운로드</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <span class="label label-warning">NEW</span>
+                <audio id="audio" preload="none" tabindex="0" controls="" src="music/Ani/Etc/2 100にゃんパワーで夢心地.mp3">
+                </audio>
+            </div>
+
+            </tbody>
+            </table>
+        </div>
+        
+		</div>
+	</body>
+</html>
